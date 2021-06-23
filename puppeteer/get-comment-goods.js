b735a7ed08204c8447d4114b11f4e215095c8526
Bowen <948266921@qq.com>
2021-06-23 10:59:24 +0800

    update goods.js

--- puppeteer/get-comment-goods.js
+++ puppeteer/get-comment-goods.js
@@ -39,8 +39,8 @@ router.get('/', async (ctx) => {
   const page = await browser.newPage();
   await page.goto('https://shopee.tw/%E4%B8%AD%E5%9C%8B%E6%98%9F%E5%B7%B4%E5%85%8B%E7%92%B0%E4%BF%9D%E5%AD%A32021%E6%9C%AA%E4%BE%86-%E5%AE%87%E5%AE%99-%E5%A4%AA%E7%A9%BA%E4%BA%BA-%E5%AE%87%E8%88%AA%E5%93%A1-%E5%A4%AA%E7%A9%BA%E5%B0%8F%E7%86%8A-%E5%AE%87%E8%88%AA-%E4%BF%9D%E6%BA%AB%E6%9D%AF-%E9%A6%AC%E5%85%8B%E6%9D%AF-i.15002216.9231816123/');
 
-  // 让页面加载完成
-  // await page.waitForTimeout(2000);
+  // 等待页面加载完成
+  await page.content();
 
   // 页面滚动到底部，让评论DOM节点加载完成才能获取dom
   await autoScrollBottom(page);
@@ -87,7 +87,7 @@ router.get('/', async (ctx) => {
     await (await page.$(COMMENT_PAGE_NEXT_BTN)).click();
 
     // 等待页面更新dom
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(1500);
 
     // 更新选择的分页
     const newPageNum = await page.evaluate(sel => {
@@ -104,6 +104,8 @@ router.get('/', async (ctx) => {
   for (const item of goodsAllData) {
     goodsInfo[item] = goodsInfo[item] ? goodsInfo[item] + 1 : 1;
   }
+
+  console.log(goodsInfo, 'goodsInfo');
 
   browser.close();
 
